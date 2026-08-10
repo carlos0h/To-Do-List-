e71b04ff1c4e5a88cf0cff08b74b2d76bf80ff1e
carlos <carlaohenriquebispojr@gmail.com>
2026-08-10 08:08:26 -0300

    corrijido logica do sistema de tachado ao concluir atividade

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const input = document.querySelector("#inputTarefa");
 const lista = document.querySelector("#listaTarefas");
 
 const botoesCheck = document.querySelectorAll(".check");
-botoesCheck.forEach(function(botao){
-    botao.addEventListener("click", function(){
-    botao.parentElement.classList.toggle("concluida");
+
+    botoesCheck.forEach(function(botao){
+        botao.addEventListener("click", function(){
+        botao.parentElement.classList.toggle("concluida");
     
     });
 });
@@ -23,6 +24,10 @@ form.addEventListener("submit", function(event){
     const botaoCheck = document.createElement("button");
     botaoCheck.classList.add("check");
     botaoCheck.textContent = "✓";
+
+    botaoCheck.addEventListener("click", function(){
+        tarefa.classList.toggle("concluida");
+    });
 
 
     const textoTarefa = document.createElement("span");
